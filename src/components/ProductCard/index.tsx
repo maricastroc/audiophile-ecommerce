02c9bb4e183ee3ac00dbb710d5e26f2a0d8e7f8d
Mariana Castro <marianacastrorc@gmail.com>
2023-08-29 22:49:01 -0300

    Refactoring

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import { Button } from '@/components/Button'
 import { Container, Heading, Label, Paragraph, TextContainer } from './styles'
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { NavbarContext } from '@/contexts/NavbarContext'
 import { useRouter } from 'next/router'
 import { ProductInfo } from '@/types/productTypes'
@@ -18,11 +18,15 @@ export default function ProductCard({
   const { screenType } = useContext(NavbarContext)
   const router = useRouter()
 
-  console.log(category)
+  const [isLoading, setIsLoading] = useState(false)
 
   function redirectToPage() {
+    setIsLoading(true)
+
     const basePath = router.basePath
     router.push(`${basePath}/products/${category}/${name}`)
+
+    setIsLoading(false)
   }
 
   return (
@@ -37,7 +41,11 @@ export default function ProductCard({
         {isNewProduct && <Label>New Product</Label>}
         <Heading>{title}</Heading>
         <Paragraph>{description}</Paragraph>
-        <Button type="primary" title="See Product" onClick={redirectToPage} />
+        <Button
+          type="primary"
+          title={isLoading ? 'Loading...' : 'See Product'}
+          onClick={redirectToPage}
+        />
       </TextContainer>
     </Container>
   )
